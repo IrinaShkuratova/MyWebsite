2860a9254b35207575a0e50a994330f15aa1b605
IrinaShkuratova <haha228228haha@gmail.com>
2025-04-06 14:34:30 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleSection(id) {
-    let section = document.getElementById(id);
-    if (section.style.display === 'block') {
-        section.style.display = 'none';
-    } else {
-        document.querySelectorAll('.section').forEach(s => s.style.display = 'none');
-        section.style.display = 'block';
-    }
-}
